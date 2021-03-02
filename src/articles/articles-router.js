04b9ad1d44cd30f14775f5eda8166bd9072a5fac
Jake Elizondo <jake.elizondo23@gmail.com>
2021-03-02 17:09:19 -0500

    ARTICLES ROUTER
    added a base route that just returns everton news

--- src/articles/articles-router.js
+++ src/articles/articles-router.js
@@ -4,6 +4,15 @@ const { TEAMCODES } = require('../TEAMS');
 
 const articlesRouter = express.Router();
 
+articlesRouter.route('/').get((req, res, next) => {
+  const page = req.query.page || 1;
+  ArticlesService.getTeamArticles(req.app.get('db'), 'EVE', page)
+    .then((articles) => {
+      return res.status(200).json(ArticlesService.serializeArticles(articles));
+    })
+    .catch(next);
+});
+
 articlesRouter.route('/:teamCode').get((req, res, next) => {
   const page = req.query.page || 1;
   // validate team
