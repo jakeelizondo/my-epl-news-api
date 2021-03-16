6f686d778a5fa422f4cad277c6792a7ec84d7685
Chris Jeon <chris0374@gmail.com>
2021-03-15 19:13:27 -0500

    Add feedback

--- src/articles/articles-service.js
+++ src/articles/articles-service.js
@@ -3,6 +3,7 @@ const { attachPaginate } = require('knex-paginate');
 attachPaginate();
 
 const ArticlesService = {
+  // Great for extracting out this sort of logic into the service.
   serializeArticle(article) {
     return {
       id: article.id,
@@ -18,10 +19,16 @@ const ArticlesService = {
     };
   },
 
+  // Great
   serializeArticles(articles) {
     return articles.map(this.serializeArticle);
   },
 
+  // This is personal preference, but query commands don't really need to be
+  // extracted out, but just put into the router itself, unless it involves
+  // very complex queries. This is simple enough to leave it in the router IMO
+  // Extracting out logic into a service can be abused at times, leading
+  // to having to open many files just to figure out what a simple query is doing.
   async getTeamArticles(db, team, page) {
     try {
       const result = await db
@@ -36,6 +43,11 @@ const ArticlesService = {
 
       return result.data;
     } catch (err) {
+      // I see what you're doing with catch statements, but there are cases
+      // where you simply want a db query to blow up. Those errors can be
+      // caught in exception monitoring tools and resolved. If you console.log
+      // it out like this, it becomes a silent error, making it easy to overlook
+      // that you have a bug in your system.
       console.log(err);
     }
   },
@@ -47,10 +59,16 @@ const ArticlesService = {
         .orderBy('published_at', 'desc');
       return result;
     } catch (err) {
+      // I see what you're doing with catch statements, but there are cases
+      // where you simply want a db query to blow up. Those errors can be
+      // caught in exception monitoring tools and resolved. If you console.log
+      // it out like this, it becomes a silent error, making it easy to overlook
+      // that you have a bug in your system.
       console.log(err);
     }
   },
 
+  // Logic like this is simple enough to just leave in the router IMO.
   insertArticle(db, article) {
     return db.insert(article).into('articles');
   },
